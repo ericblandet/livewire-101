3e9f6bad10df850ceef2e733161b65a3a6c6b326
eric <eric.blandet@gmail.com>
2023-04-17 14:40:14 +0200

    rework boilerplate

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,17 +1,3 @@
-<!--
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 <form class="mx-auto max-w-xl">
 
 
@@ -34,14 +20,14 @@
                 <div class="mt-2">
                     <input
                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        id="email" name="email" type="email" autocomplete="email">
+                        id="email" name="email" type="email" autocomplete="email" required>
                 </div>
             </div>
             <div class="mt-3 sm:col-span-full">
                 <label class="block text-sm font-medium leading-6 text-gray-900" for="phone">Phone</label>
                 <input
                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    id="phone" name="phone" type="tel">
+                    id="phone" name="phone" type="tel" required>
             </div>
             <div class="col-span-full mt-3">
                 <label class="block text-sm font-medium leading-6 text-gray-900" for="about">About</label>
@@ -60,7 +46,6 @@
 
 
     <div class="mt-6 flex items-center justify-end gap-x-6">
-        <button class="text-sm font-semibold leading-6 text-gray-900" type="button">Cancel</button>
         <button
             class="rounded-md bg-gradient-to-r from-amber-300 to-pink-300 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             type="submit">Save</button>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,14 +17,20 @@
 
 <body class="antialiased">
     <div>
-
-        <div class="mx-auto max-w-3xl">
+        <div class="mx-auto mt-10 mb-10 max-w-3xl">
+            <section
+                class="flex h-96 items-center justify-center rounded-2xl bg-gradient-to-tr from-fuchsia-500 to-cyan-500">
+                <h1 class="text block text-9xl italic text-slate-600">Placeholder</h1>
+            </section>
+            <section
+                class="flex h-96 items-center justify-center rounded-2xl bg-gradient-to-tr from-fuchsia-500 to-cyan-500">
+                <h1 class="text block text-9xl italic text-slate-600">Placeholder</h1>
+            </section>
 
             <section>
                 <x-form></x-form>
             </section>
         </div>
-
     </div>
 </body>
 
